6c552bb93c948fc00987fd5dcf099cd1721a2052
parkjei <parkjei94@gmail.com>
2023-08-18 17:28:38 +0900

    Refactor-code-refactoring&Design-edit-resume-modal-ui #6
    1. 코드 리팩토링
    2. 지원한 프리랜서 확인하기 모달 내용 추가 및 UI 수정
    
     줄은 무시되고, 메시지를 입력하지 않으면 커밋이 중지됩니다.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import FreelancerList from "../components/home/freelancerList/FreelancerList";
 import ApplicantFreelancerLists from "../components/home/ApplicantFreelancerLists";
 // 마켓플레이스 페이지
