5007236081c48a898839de5d0d1f7c733c05b108
Lee Jinsol <jins2ol@naver.com>
2023-08-18 20:29:59 +0900

    Feat-add Menu Tab Bar
    프로젝트 관리 도구 및 진행상태 메뉴바 생성

--- src/components/task/TaskStyles.ts
+++ src/components/task/TaskStyles.ts
@@ -8,14 +8,33 @@ interface TaskDetailBoxProps {
 const S = {
   TaskCardContainer: styled.div`
     display: flex;
+    align-items: center;
   `,
   TaskDetailBox: styled.div<TaskDetailBoxProps>`
     background-color: ${(props) =>
       props.backgroundColor ? props.backgroundColor : "#d3d3d3"};
     width: ${(props) => props.width}px;
-    height: 70px;
-    margin: 5px;
+    height: 50px;
+    margin: 5px 10px 5px 0;
+    border-radius: 3px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
   `,
+  TaskDeleteButton: styled.button`
+    background-color: transparent;
+    border: none;
+  `,
+  TaskTitleInput: styled.input`
+    height: 50px;
+    width: 200px;
+    text-align: center;
+  `,
+  // StatusOptionBox: styled.div`
+  //   position: absolute;
+  //   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+  // `,
 };
 
 export default S;
--- src/pages/ProjectManagement.tsx
+++ src/pages/ProjectManagement.tsx
@@ -1,12 +1,57 @@
+import { styled } from "styled-components";
 import TaskBox from "../components/task/TaskBox";
+import { useState } from "react";
 
 //프로젝트 관리 도구: 여러 프로젝트의 진행 상황, 마감일 및 작업 항목을 한눈에 볼 수 있게 함
 const ProjectManagement = () => {
+  const [isTaskTabOn, setIsTaskTabOn] = useState(true);
   return (
-    <div>
-      <TaskBox />
-    </div>
+    <>
+      <S.MenuTabBar>
+        <S.MenuTab
+          onClick={() => setIsTaskTabOn(true)}
+          fontWeight={isTaskTabOn ? 600 : 400}
+        >
+          프로젝트 진행 상태
+        </S.MenuTab>
+        <S.MenuTab
+          onClick={() => setIsTaskTabOn(false)}
+          fontWeight={isTaskTabOn ? 400 : 600}
+        >
+          프로젝트 관리
+        </S.MenuTab>
+      </S.MenuTabBar>
+      <S.CompContainer>
+        {isTaskTabOn ? <TaskBox /> : <div>프로젝트 관리</div>}
+      </S.CompContainer>
+    </>
   );
 };
 
 export default ProjectManagement;
+
+interface MenuTabProps {
+  fontWeight?: number;
+}
+
+const S = {
+  MenuTabBar: styled.div`
+    width: 100%;
+    height: 70px;
+    border-bottom: 1px solid grey;
+
+    display: flex;
+    align-items: center;
+
+    padding: 0 20px;
+  `,
+  MenuTab: styled.span<MenuTabProps>`
+    margin: 0 20px;
+    font-size: 15px;
+    font-weight: ${(props) => props.fontWeight};
+    cursor: pointer;
+  `,
+  CompContainer: styled.div`
+    padding: 30px;
+  `,
+};
