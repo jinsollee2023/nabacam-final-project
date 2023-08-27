9b1a459d2332cd81c3daaf9bb5b6d650826571f2
Lee Jinsol <jins2ol@naver.com>
2023-08-27 22:31:09 +0900

    feat - add nav func
    현재 로그인된 유저 정보 네비바에 출력
    유저 롤에 따라서 네브바 메뉴 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,27 @@
+import { useUserStore } from "src/zustand/useUserStore";
 import MenuTabBarComp from "../components/common/MenuTabBarComp";
 
 // 프리랜서 구인 페이지
 
 const Home = () => {
+  const { user } = useUserStore();
   const freelancerOfferMenu = [
     "프리랜서 마켓",
     "지원한 프리랜서 확인",
     "보류한 프리랜서",
   ];
+  const exploreProjectMenu = [
+    "프로젝트 탐색",
+    "지원한 프로젝트",
+    "제안받은 프로젝트",
+  ];
 
   return (
     <>
-      <MenuTabBarComp menu={freelancerOfferMenu} />
+      {user.role === "client" && <MenuTabBarComp menu={freelancerOfferMenu} />}
+      {user.role === "freelancer" && (
+        <MenuTabBarComp menu={exploreProjectMenu} />
+      )}
     </>
   );
 };
