cd223de97bd9b2905bd76b0885d7b2ad130d9eda
parkjei <parkjei94@gmail.com>
2023-08-31 18:46:31 +0900

    Design-ongoing freelancer card, contract termination card ui #108 - 진행중인 프리랜서, 계약이 끝난 프리랜서 카드 UI 작업

--- src/components/myPage/client/listOfFreelancersByStatus/listOfFreelancersByStatusStyle.ts
+++ src/components/myPage/client/listOfFreelancersByStatus/listOfFreelancersByStatusStyle.ts
@@ -15,7 +15,7 @@ export const S = {
     height: 30px;
     background-color: transparent;
     border: 2px solid #0086d0;
-    border-radius: 10px;
+    border-radius: 4px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -29,10 +29,10 @@ export const S = {
     overflow: auto;
   `,
   ListsBox: styled.div`
-    border: 2px solid #d6d6d6;
-    border-radius: 5px;
-    width: 30%;
-    height: 350px;
+    border: 1.5px solid #d6d6d6;
+    border-radius: 4px;
+    width: 340px;
+    height: 420px;
     margin-bottom: 20px;
     display: table;
   `,
@@ -62,16 +62,23 @@ export const S = {
     font-weight: bold;
   `,
   WorkField: styled.span`
+    font-size: 16px;
     font-weight: bold;
-    color: gray;
+    color: #3d3d3d;
   `,
   WorkSmallFieldAndWorkExp: styled.span`
-    color: gray;
+    font-size: 14px;
+    color: #595959;
   `,
   ContactBox: styled.div`
+    gap: 10px;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     align-items: center;
+    margin-top: 5px;
+  `,
+  Contact: styled.span`
+    font-size: 14px;
   `,
 
   Line: styled.hr`
@@ -80,25 +87,31 @@ export const S = {
     border: 0;
     background: #0086d0;
   `,
-  OngoingProject: styled.span`
+  ProjectTitle: styled.span`
     font-weight: bold;
     font-size: 20px;
   `,
-  ProjectTitle: styled.span`
-    font-size: 18px;
+  ProjectSubTitle: styled.span`
+    font-size: 16px;
   `,
-  ProjectDate: styled.span``,
+  ProjectDate: styled.span`
+    font-size: 14px;
+  `,
   DateInnerText: styled.span`
-    color: gray;
+    color: #595959;
     font-size: 12px;
   `,
   DetailBtn: styled.button`
+    position: relative;
+    top: 50px;
     border: 1px solid #0086d0;
-    border-radius: 5px;
+    border-radius: 4px;
     width: 90%;
-    height: 35px;
+    max-width: 300px;
+    height: 44px;
     background-color: transparent;
     font-size: 16px;
     cursor: pointer;
+    color: #0086d0;
   `,
 };
