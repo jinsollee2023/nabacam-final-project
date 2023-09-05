233e13c11d022b933e4707e4a6dff824cc4ca503
AJ3504 <plzgimmeajob@gmail.com>
2023-09-05 10:24:40 +0900

    "Refactor/ResumeTab구조변경, .maybeSingle()변경, any삭제"

--- src/components/myPage/tabs/resumeTab/ResumeTab.tsx
+++ src/components/myPage/tabs/resumeTab/ResumeTab.tsx
@@ -1,8 +1,8 @@
 import { styled } from "styled-components";
-import ResumeExperienceComp from "./ResumeExperienceComp";
-import ResumeProfileIntroComp from "./ResumeProfileIntroComp";
 import React from "react";
 import { S } from "./Resume.styles";
+import ResumeProfileIntroComp from "./profileIntro/ResumeProfileIntroComp";
+import ResumeExperienceComp from "./experience/ResumeExperienceComp";
 
 const ResumeTab = () => {
   return (
--- src/components/myPage/tabs/resumeTab/experience/ResumeExperienceCard.tsx
+++ src/components/myPage/tabs/resumeTab/experience/ResumeExperienceCard.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Modal from "../../../../components/modal/Modal";
-import useResumeExperienceQueries from "../../../../hooks/useResumeExperienceQueries";
-import { useUserStore } from "../../../../zustand/useUserStore";
-import type { ResumeExperience } from "../../../../Types";
-import { useResumeExperienceStore } from "../../../../zustand/useResumeExperienceStore";
+import Modal from "../../../../../components/modal/Modal";
+import useResumeExperienceQueries from "../../../../../hooks/useResumeExperienceQueries";
+import { useUserStore } from "../../../../../zustand/useUserStore";
+import type { ResumeExperience } from "../../../../../Types";
+import { useResumeExperienceStore } from "../../../../../zustand/useResumeExperienceStore";
 import EditResumeExperienceModal from "./EditResumeExperienceModal";
-import { S } from "./Resume.styles";
+import { S } from "../Resume.styles";
 import { CommonS } from "src/components/common/button/commonButton";
 
 interface ExperienceProps {
