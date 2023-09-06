342c683ce84a28784e38acb970d09292a849bf49
jung <wjdtmdgk55@gmail.com>
2023-09-06 18:16:12 +0900

    동기화

--- src/components/auth/login/LoginComp.styles.ts
+++ src/components/auth/login/LoginComp.styles.ts
@@ -6,7 +6,10 @@ export const S = {
     width: 100%;
     height: 40%;
     border-radius: 10px;
-    background-color: #dbcfcf;
+    background-color: #f0f0f0;
+    padding: 0 10px;
+    outline: none;
+    border: none;
   `,
   LoginBack: styled.div`
     align-items: center;
@@ -16,55 +19,30 @@ export const S = {
   `,
   Loginfont: styled.h1`
     width: 100%;
-    height: 48px;
     margin-bottom: 5%;
     text-align: center;
     font-weight: 284px;
-    font-size: 48px;
+    font-size: 40px;
   `,
   LoginButton: styled.button`
-    width: 80%;
+    width: 100%;
     height: 50px;
-    margin-left: 10%;
-    margin-top: 15%;
+    margin-top: 10%;
     border-radius: 10px;
     font-weight: 62px;
-    font-size: 28px;
-    background-color: black;
+    font-size: 20px;
+    background-color: var(--main-blue);
+    border: none;
     color: white;
     cursor: pointer;
-    box-shadow: 2px 2px 2px gray;
   `,
   LoginBG: styled.div`
     position: relative;
     top: 20%;
     left: 45%;
-
     width: 40%;
     height: 50%;
     border-radius: 10px;
-  `,
-  passwordView: styled.button`
-    position: relative;
-    top: -41.5%;
-    left: 94%;
-    width: 5%;
-    height: 5%;
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    border-radius: 10px;
-  `,
-  errordiv: styled.div`
-    height: 20px;
-  `,
-  passwordFindButton: styled.button`
-    width: 100%;
-    height: 43px;
-    border-radius: 10px;
-    border: none;
-    cursor: pointer;
-    background-color: white;
   `,
   PasswordInputWrapper: styled.div`
     display: flex;
@@ -75,7 +53,7 @@ export const S = {
     width: 93%;
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
-    background-color: #dbcfcf;
+    background-color: #f0f0f0;
     padding: 0 10px;
     outline: none;
   `,
@@ -87,10 +65,105 @@ export const S = {
     width: 7%;
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
-    background-color: #dbcfcf;
+    background-color: #f0f0f0;
   `,
   EyeBtn: styled.button`
     border: none;
     background-color: transparent;
   `,
+
+  errordiv: styled.div`
+    height: 20px;
+  `,
+  passwordFindButton: styled.button`
+    width: 100%;
+    height: 43px;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+    background-color: white;
+  `,
 };
+
+// const S = {
+//   LoginInput: styled.input`
+//     align-items: center;
+//     width: 100%;
+//     height: 40%;
+//     border-radius: 10px;
+//     background-color: #F0F0F0;
+//     padding: 0 10px;
+//     outline: none;
+//     border: none;
+//   `,
+//   LoginBack: styled.div`
+//     align-items: center;
+//     justify-content: center;
+//     width: 100%;
+//     height: 120px;
+//   `,
+//   Loginfont: styled.h1`
+//     width: 100%;
+//     margin-bottom: 5%;
+//     text-align: center;
+//     font-weight: 284px;
+//     font-size: 40px;
+//   `,
+//   LoginButton: styled.button`
+//     width: 100%;
+//     height: 50px;
+//     margin-top: 10%;
+//     border-radius: 10px;
+//     font-weight: 62px;
+//     font-size: 20px;
+//     background-color: var(--main-blue);
+//     border: none;
+//     color: white;
+//     cursor: pointer;
+//   `,
+//   LoginBG: styled.div`
+//     position: relative;
+//     top: 20%;
+//     left: 45%;
+//     width: 40%;
+//     height: 50%;
+//     border-radius: 10px;
+//   `,
+//   PasswordInputWrapper: styled.div`
+//     display: flex;
+//     height: 40%;
+//   `,
+//   PasswordInput: styled.input`
+//     border: none;
+//     width: 93%;
+//     border-top-left-radius: 10px;
+//     border-bottom-left-radius: 10px;
+//     background-color: #F0F0F0;
+//     padding: 0 10px;
+//     outline: none;
+//   `,
+//   CenterizeBox: styled.div`
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     width: 7%;
+//     border-top-right-radius: 10px;
+//     border-bottom-right-radius: 10px;
+//     background-color: #F0F0F0;
+//   `,
+//   EyeBtn: styled.button`
+//     border: none;
+//     background-color: transparent;
+//   `,
+//   errordiv: styled.div`
+//     height: 20px;
+//   `,
+//   passwordFindButton: styled.button`
+//     width: 100%;
+//     height: 43px;
+//     border-radius: 10px;
+//     border: none;
+//     cursor: pointer;
+//     background-color: white;
+//   `,
+// };
