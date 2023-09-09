8fd6337649689dfcf9f0a95da42feeaece86e61e
Lee Jinsol <jins2ol@naver.com>
2023-09-09 17:33:23 +0900

    feat - member validation
    구성원 추가 및 수정 밸리데이션 체크 기능 추가

--- src/components/myPage/client/AddMemberModal.tsx
+++ src/components/myPage/client/AddMemberModal.tsx
@@ -3,40 +3,48 @@ import { Member } from "src/Types";
 import { S } from "./addMemberModal.style";
 import { formatPhoneNumber } from "src/components/common/commonFunc";
 import { useUserStore } from "src/store/useUserStore";
+import useValidation from "src/hooks/useValidation";
+import { Errors } from "./MemberList";
 
 interface AddMemberModalProps {
   currentMemberData?: Member;
   setUpdateMemberData: (updatdMemberData: Member) => void;
+  errors: Errors;
+  setErrors: (errors: Errors) => void;
 }
 
 const AddMemberModal = ({
   currentMemberData,
   setUpdateMemberData,
+  errors,
+  setErrors,
 }: AddMemberModalProps) => {
-  const [name, setName] = useState(
-    currentMemberData ? currentMemberData.name : ""
-  );
-  const [team, setTeam] = useState(
-    currentMemberData ? currentMemberData.team : ""
-  );
-  const [email, setEmail] = useState(
-    currentMemberData ? currentMemberData.contact.email : ""
-  );
-  const [phone, setPhone] = useState(
-    currentMemberData ? currentMemberData.contact.phone : ""
-  );
   const { user } = useUserStore();
+  const initialValues = {
+    name: currentMemberData ? currentMemberData.name : "",
+    team: currentMemberData ? currentMemberData.team : "",
+    email: currentMemberData ? currentMemberData.contact.email : "",
+    phone: currentMemberData ? currentMemberData.contact.phone : "",
+  };
+  const [values, setValues] = useState(initialValues);
+
+  const { validateName, validateTeam, validateEmail, validatePhone } =
+    useValidation();
+
+  const handleChange = (key: string, value: string) => {
+    setValues({ ...values, [key]: value });
+  };
 
   useEffect(() => {
     setUpdateMemberData({
-      name,
-      team,
+      name: values.name,
+      team: values.team,
       contact: {
-        email,
-        phone,
+        email: values.email,
+        phone: values.phone,
       },
     });
-  }, [name, team, email, phone]);
+  }, [values]);
 
   return (
     <S.AddMemberBox>
@@ -48,35 +56,65 @@ const AddMemberModal = ({
       </S.ClientInfo>
       <S.WriteBox>
         <S.Label htmlFor="memberName">이름</S.Label>
-        <S.Input
-          id="memberName"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <div>
+          <S.Input
+            id="memberName"
+            value={values.name}
+            onChange={(e) => handleChange("name", e.target.value)}
+            onBlur={(e) => {
+              const nameError = validateName(e.target.value);
+              setErrors({ ...errors, name: nameError });
+            }}
+          />
+          {errors.name && <p>{errors.name}</p>}
+        </div>
       </S.WriteBox>
       <S.WriteBox>
         <S.Label htmlFor="memberPhone">전화번호</S.Label>
-        <S.Input
-          id="memberPhone"
-          value={phone}
-          onChange={(e) => setPhone(formatPhoneNumber(e.target.value))}
-        />
+        <div>
+          <S.Input
+            id="memberPhone"
+            value={values.phone}
+            onChange={(e) =>
+              handleChange("phone", formatPhoneNumber(e.target.value))
+            }
+            onBlur={(e) => {
+              const phoneError = validatePhone(e.target.value);
+              setErrors({ ...errors, phone: phoneError });
+            }}
+          />
+          {errors.phone && <p>{errors.phone}</p>}
+        </div>
       </S.WriteBox>
       <S.WriteBox>
         <S.Label htmlFor="memberEmail">이메일</S.Label>
-        <S.Input
-          id="memberEmail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <div>
+          <S.Input
+            id="memberEmail"
+            value={values.email}
+            onChange={(e) => handleChange("email", e.target.value)}
+            onBlur={(e) => {
+              const emailError = validateEmail(e.target.value);
+              setErrors({ ...errors, email: emailError });
+            }}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
       </S.WriteBox>
       <S.WriteBox>
         <S.Label htmlFor="memberTeam">담당 부서</S.Label>
-        <S.Input
-          id="memberTeam"
-          value={team}
-          onChange={(e) => setTeam(e.target.value)}
-        />
+        <div>
+          <S.Input
+            id="memberTeam"
+            value={values.team}
+            onChange={(e) => handleChange("team", e.target.value)}
+            onBlur={(e) => {
+              const teamError = validateTeam(e.target.value);
+              setErrors({ ...errors, team: teamError });
+            }}
+          />
+          {errors.team && <p>{errors.team}</p>}
+        </div>
       </S.WriteBox>
     </S.AddMemberBox>
   );
