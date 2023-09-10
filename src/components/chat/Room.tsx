80dce6479e95b4450f92343fa0c8a0a5d07bd9d8
AJ3504 <plzgimmeajob@gmail.com>
2023-09-10 17:51:17 +0900

    "save/room separated/still need fix"

--- src/components/chat/Room.tsx
+++ src/components/chat/Room.tsx
@@ -5,7 +5,7 @@ import { useUserStore } from "../../zustand/useUserStore";
 import { toast } from "react-toastify";
 import { useParams } from "react-router-dom";
 
-interface Room {
+export interface Room {
   room_id: string;
   created_at: string;
   roomname: string | null;
@@ -31,11 +31,38 @@ const Room = () => {
     }
   };
 
+  const handleInvite = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      // 입력값
+      const target = e.currentTarget;
+      // 입력값 -> find matching user in users
+      const { data } = await supabase
+        .from("users")
+        .select("userId")
+        .match({ name: target.value })
+        .single();
+      console.log(data);
+      if (!data) return toast.error("No User found");
+      // matching user -> room_participants
+      const { error } = await supabase
+        .from("room_participants")
+        .insert({ user_id: data.userId, room_id: room_id }); // policy
+      if (error) return toast.error(error.message);
+
+      target.value = "";
+    }
+  };
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <section className="flex h-screen flex-col items-center justify-center">
       <div className="flex h-full w-full flex-1 flex-col items-stretch py-10 px-20 text-gray-800">
         {/* 제목 */}
-        <h1 className="bg-yellow-100 px-4 py-2 text-4xl">00 Room</h1>
+        <div className="bg-yellow-100 px-4 py-2 flex justify-between">
+          <h1 className="text-4xl">00 Room</h1>
+          {/*  */}
+          <input type="text" onKeyPress={handleInvite} />
+        </div>
+
         {/* 본문 */}
         {room_id && <Messages room_id={room_id} />}
         {/* 창 */}
@@ -47,7 +74,7 @@ const Room = () => {
           />
         </form>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Messages from "../components/chat/Messages";
 import supabase from "../config/supabaseClient";
 import { useUserStore } from "../zustand/useUserStore";
 import { styled } from "styled-components";
 import { toast } from "react-toastify";
+import { Room } from "src/components/chat/Room";
 
 const Chat = () => {
   const { user } = useUserStore();
   const userId = user.userId; // users테이블의 userId를 user_id컬럼에 삽입, rpc에도 삽입
   const navigate = useNavigate();
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  useEffect(() => {
+    const getRooms = async () => {
+      const { data } = await supabase
+        .from("rooms")
+        .select("*")
+        .order("created_at", { ascending: false }); // 가장 최신순 맨 위에
+      console.log(data);
+      if (data) setRooms(data);
+    };
+    getRooms();
+  }, []);
 
   const handleCreateRoom = async () => {
     const { data, error } = await supabase.rpc("create_room", {
@@ -28,16 +42,21 @@ const Chat = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="flex h-full w-full flex-1 flex-col items-stretch bg-blue-400 py-10 px-20 text-gray-800">
-        {/* 제목 */}
         <h1 className="bg-green-200 px-4 py-2 text-4xl">
-          Work Wave Chat
+          {/* 1.제목 */}
+          <p>
+            <Link to={"/chat"}>
+              <a>Work Wave Chat</a>
+            </Link>
+          </p>
+          {/* 2.enter individual room */}
           <button
             onClick={handleCreateRoom}
             className="ml-4 rounded border bg-red-200 p-2 text-xs"
           >
             New room
           </button>
-          {/*  */}
+          {/* 3.enter openchat room */}
           <button
             className="ml-4 rounded border bg-red-200 p-2 text-xs"
             onClick={() =>
@@ -47,6 +66,18 @@ const Chat = () => {
             Open Chat
           </button>
         </h1>
+        {/* room lists */}
+        <div className="flex-1 bg-gray-100 p-4">
+          {rooms.map((room) => (
+            <div key={room.room_id}>
+              <p>
+                <Link to={`/chat/${room.room_id}`}>
+                  <a>{room.roomname ?? "Untitled"}</a>
+                </Link>
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
