36eb210c79a1c2337be7d989869d1469eb582240
jung <wjdtmdgk55@gmail.com>
2023-09-12 23:27:36 +0900

    간단한수정

--- src/components/modal/freelancerInfo/FreelancerProfile.tsx
+++ src/components/modal/freelancerInfo/FreelancerProfile.tsx
@@ -7,6 +7,8 @@ import { toast } from "react-toastify";
 import useClientsQueries from "src/hooks/useClientsQueries";
 import { useUserStore } from "src/store/useUserStore";
 import useProjectOfFreelancerBySortQueries from "src/hooks/queries/useProjectOfFreelancerBySortQueries";
+import useProjectsQueries from "src/hooks/useProjectsQueries";
+import { useState } from "react";
 
 interface FreelancerProfileProps {
   user: IUser;
@@ -14,12 +16,15 @@ interface FreelancerProfileProps {
 
 const FreelancerProfile = ({ user }: FreelancerProfileProps) => {
   // 클릭 시 텍스트 클립보드에 복사하기 위해 생성
+
+  const [selectedSortLabel, setSelectedSortLabel] = useState("전체보기");
   const { userId } = useUserStore();
 
-  const { projectsListBySort } = useProjectOfFreelancerBySortQueries({
+  const { projectsOfClient } = useProjectsQueries({
     currentUserId: userId,
+    sortLabel: selectedSortLabel,
   });
-
+  console.log("projectsOfClient==>", projectsOfClient);
   const handleCopyClipBoard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
@@ -28,14 +33,9 @@ const FreelancerProfile = ({ user }: FreelancerProfileProps) => {
       console.log(err);
     }
   };
-  const projectItem = projectsListBySort
-    ?.filter((projectItem) => {
-      return projectItem.clientId === userId;
-    })
-    .filter((checkItem) => {
-      return checkItem.freelancerId === user.userId;
-    });
-  const check = projectItem?.length;
+  const projectItem = projectsOfClient?.find((item) => {
+    return item.freelancerId === user.userId;
+  });
 
   return (
     <>
@@ -50,24 +50,24 @@ const FreelancerProfile = ({ user }: FreelancerProfileProps) => {
             <span>{user.workField?.workSmallField}</span>
             <span>{user.workExp}년차</span>
           </S.WorkSmallFieldAndWorkExp>
-          <S.ContactBox>
-            {check !== 0 && (
-              <S.Contacts
-                onClick={() => handleCopyClipBoard(`${user.contact.phone}`)}
-              >
-                <FiPhoneCall size={18} /> {user.contact.phone}
-              </S.Contacts>
-            )}
-          </S.ContactBox>
-          <S.ContactBox>
-            {check !== 0 && (
-              <S.Contacts
-                onClick={() => handleCopyClipBoard(`${user.contact.email}`)}
-              >
-                <FiMail size={18} /> {user.contact.email}
-              </S.Contacts>
-            )}
-          </S.ContactBox>
+          {projectItem && (
+            <>
+              <S.ContactBox>
+                <S.Contacts
+                  onClick={() => handleCopyClipBoard(`${user.contact.phone}`)}
+                >
+                  <FiPhoneCall size={18} /> {user.contact.phone}
+                </S.Contacts>
+              </S.ContactBox>
+              <S.ContactBox>
+                <S.Contacts
+                  onClick={() => handleCopyClipBoard(`${user.contact.email}`)}
+                >
+                  <FiMail size={18} /> {user.contact.email}
+                </S.Contacts>
+              </S.ContactBox>
+            </>
+          )}
         </S.UserBox>
       </S.UserInfoBox>
       <S.UnderLine />
