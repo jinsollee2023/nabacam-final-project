26b0c118dbeefcfd963b23117ee764436a1957be
AJ3504 <plzgimmeajob@gmail.com>
2023-09-18 00:49:55 +0900

    "cleared 주석"

--- src/components/common/commonFunc.ts
+++ src/components/common/commonFunc.ts
@@ -49,38 +49,7 @@ export const formatPhoneNumber = (phoneNumber: string) => {
   return phoneNumberFormat;
 };
 
-/** 모달 공통함수 : sendDM */
-
-// const handleCreateRoom = async ({
-//   DMfreelancerName,
-//   DMclientName,
-//   DMfreelancerId,
-//   DMclientId,
-//   setCreatedRoomId,
-//   setSelectedRoom,
-// }: {
-//   DMfreelancerName: string;
-//   DMclientName: string;
-//   DMfreelancerId: string;
-//   DMclientId: string;
-//   setCreatedRoomId: (roomId: string) => void;
-//   setSelectedRoom: (room: TRoom | null) => void;
-// }) => {
-//   // const { setCreatedRoomId, setSelectedRoom } = useRoomStore();
-//   // 방 생성 + 구성원 집어넣음
-//   const { data, error } = await supabase.rpc("create_room2", {
-//     roomname: `${DMfreelancerName}, ${DMclientName}`,
-//     user_id: DMfreelancerId,
-//     receiver_id: DMclientId,
-//   });
-
-//   if (data) {
-//     const room_id = data.room_id;
-//     setCreatedRoomId(room_id);
-//     setSelectedRoom(data);
-//   }
-// };
-
+/** sendDM 공통함수 */
 const checkDuplicateRoomId = async ({
   DMfreelancerId,
   DMclientId,
@@ -146,54 +115,3 @@ export const sendDM = async ({
   }
   navigate("/chat");
 };
-
-//=======================================//
-// const handleCreateRoom = async () => {
-//   // 방 생성 + 구성원 집어넣음
-//   const { data, error } = await supabase.rpc("create_room2", {
-//     roomname: `${clientName}, ${freelancerName}`,
-//     user_id: clientId,
-//     receiver_id: freelancerId,
-//   });
-//   if (error) {
-//     toast.error(error.message);
-//     return;
-//   }
-//   if (data) {
-//     const room_id = data.room_id;
-//     setCreatedRoomId(room_id);
-//     setSelectedRoom(data);
-//   }
-// };
-
-// const checkDuplicateRoomId = async () => {
-//   // c -> f 인 room_id 찾기
-//   const { data, error } = await supabase
-//     .from("room_participants")
-//     .select("room_id")
-//     .match({ receiver_id: freelancerId, user_id: clientId })
-//     .single();
-//   // console.log("67", data);
-//   return data ? data.room_id : null;
-// };
-
-// // 클릭 시 해당 프리랜서에게 DM 전송
-// const sendDM = async () => {
-//   // 중복 방 여부 확인
-//   const result = await checkDuplicateRoomId();
-//   // console.log("result", result);
-
-//   if (result !== null) {
-//     // console.log(
-//     //   "이미 생성된 방이 있습니다. 해당 채팅방으로 이동은 구현중입니다."
-//     // );
-
-//     navigate("/chat");
-//     return;
-//   }
-
-//   // 중복 없을 경우 새로운 방 생성
-//   // console.log("채팅 내역이 없습니다.");
-//   handleCreateRoom();
-//   navigate("/chat");
-// };
