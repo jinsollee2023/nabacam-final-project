df20fc0dcb0bd972cf5c22caade6ad79fbb22ba7
Lee Hanbyeol <leehanbyeol971219@gmail.com>
2023-09-17 22:01:30 +0900

    Fix-sendDM button position
    문의하기 버튼 위치 수정 및 없는 모달에 추가

--- src/components/home/freelancerMarket/freelancerList/FreelancerCard.tsx
+++ src/components/home/freelancerMarket/freelancerList/FreelancerCard.tsx
@@ -375,13 +375,13 @@ const FreelancerCard = ({
               setIsModalOpen={setIsInfoModalOpen}
               buttons={
                 <>
-                  <S.FreelancerInfoModalButton onClick={sendDM}>
-                    문의하기
-                  </S.FreelancerInfoModalButton>
                   <S.FreelancerInfoModalButton
                     onClick={handleInfoModalProposalButtonClick}
                   >
                     제안하기
+                  </S.FreelancerInfoModalButton>
+                  <S.FreelancerInfoModalButton onClick={sendDM}>
+                    문의하기
                   </S.FreelancerInfoModalButton>
                 </>
               }
